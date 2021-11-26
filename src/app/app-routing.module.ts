8c5197db0d040c1e59814e6f61e34ef43c26815f
admiralpxl <wnstnjr@gmail.com>
2021-11-26 14:17:14 -0500

    pages component created and routes made it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { EmpresasComponent } from './pages/empresas/empresas.component';
+import { ArticulosComponent } from './pages/articulos/articulos.component';
 
+const routes: Routes = [
+  {
+    path: '/',
+    component: HomeComponent,
+  },
+  {
+    path: 'empresas',
+    component: EmpresasComponent,
+  },
+  {
+    path: 'articulos',
+    component: ArticulosComponent,
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
