cd274379a593752429fd347d351916e01e8a7c64
admiralpxl <wnstnjr@gmail.com>
2021-11-26 16:55:32 -0500

    footer and header finished

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  menuOn: boolean = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  showMenu() {
+    if (this.menuOn) {
+      this.menuOn = false;
+    } else {
+      this.menuOn = true;
+    }
+    console.log(this.menuOn);
   }
 
+  ngOnInit(): void {}
 }
