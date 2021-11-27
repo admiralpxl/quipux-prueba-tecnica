024d7c36d5418aceee130029696d9e7a126391e6
admiralpxl <wnstnjr@gmail.com>
2021-11-26 23:31:19 -0500

    challenge finishes

--- src/app/components/user-login/user-login.component.ts
+++ src/app/components/user-login/user-login.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.css']
+  styleUrls: ['./user-login.component.css'],
 })
 export class UserLoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  user = {
+    name: 'Junior Carrillo',
+    img: 'https://avatars.githubusercontent.com/u/78501030?s=40&v=4',
+  };
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
